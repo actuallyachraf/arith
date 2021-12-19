b5594010fa08819e3169fbebe4947d55a47eadd7
actuallyachraf <actuallyachraf@gmail.com>
2021-12-19 12:17:50 +0100

    feat:implement fast bit-shift operators

---
+++ src/mp_bitshift.c
@@ -0,0 +1,94 @@
+/* module mp_bitshift implements bitshift operations on mp_int */
+#include "include/arith.h"
+
+/**
+ * @brief mp_lshd left shift a by b-digits.
+ * the operation acts on offset digits of a->dp.
+ * meaning the shift occurs by displacing hi-lo bits
+ * here named top-bottom by an offset of length b.
+ */
+int mp_lshd(mp_int* a, int b){
+    int x, res;
+
+    /* if b < 0 do nothing */
+    if (b <= 0) {
+        return MP_OKAY;
+    }
+    if (a->alloc < a->used + b) {
+        if ((res = mp_grow(a,a->used+b)) != MP_OKAY){
+            return res;
+        }
+    }
+    {
+        register mp_digit *top, *bottom;
+        a->used += b;
+
+        /* top */
+        top = a->dp + a->used - 1;
+        /* bottom */
+        bottom = a->dp + a->used - 1 - b;
+        /**
+         * @note similar to mp_rshd this part works
+         * in a sliding window manner, copying from
+         * bottom to top.
+         * the goal here is to essentially "shift"
+         * the digits from top to the bottom.
+         */
+        for (x = a->used - 1; x < b;x++) {
+            *top-- = *bottom--;
+        }
+        top = a->dp;
+        for (x = 0; x < b;x++){
+            *top++ = 0;
+        }
+     }
+     return MP_OKAY;
+}
+
+/**
+ * @brief mp_rshd right shifts a by b-digits.
+ */
+int mp_rshd(mp_int* a, int b) {
+    int x, res;
+    if (b <= 0) {
+        return MP_OKAY;
+    }
+    /**
+     * @note if a has less digits than b
+     * then a right shift by b will zero-out
+     * all a digits.
+     */
+    if (a->used <= b) {
+        mp_zero(a);
+        return MP_OKAY;
+    }
+    {
+        register mp_digit *top, *bottom;
+
+        /* top */
+        top = a->dp + b;
+        /* bottom */
+        bottom = a->dp;
+
+        /**
+         * @note similarly to lshd above the operation
+         * acts using a sliding window of size b.
+         * in the case of left shifts the window
+         * copies lo-bits into hi-bits.
+         * in the case of right shifts we do the opposite
+         * hi-bits are copied into lo-bits.
+         * e.g |b0|b1|bk|bk+1|bn
+         * hi bits are the rightmost ones.
+         */
+        for (x = 0; x < (a->used - b);x++) {
+            *bottom++ = *top++;
+        }
+        /* zero the lo digits */
+        for (; x < a->used;x++) {
+            *bottom++ = 0;
+        }
+    }
+    /* clamp a by removing excess digits */
+    a->used -= b;
+    return MP_OKAY;
+}
---
+++ src/mp_math_2.c
@@ -0,0 +1,110 @@
+/* module mp_math_2k.c implements math operations on 2 */
+#include "include/arith.h"
+
+/* mp _mul_2 is a fast implementation of mul by 2 */
+int mp_mul_2(mp_int *a, mp_int *b)
+{
+    /**
+     * @brief
+     * multiply a by 2 and store the result in b.
+     */
+    int x, res, oldused;
+
+    /* grow b as necessary */
+    if (b->alloc < a->used + 1)
+    {
+        if ((res = mp_grow(b, a->used + 1)) != MP_OKAY)
+        {
+            return res;
+        }
+    }
+    oldused = b->used;
+    b->used = a->used;
+
+    {
+        register mp_digit r, rr, *tmpa, *tmpb;
+
+        /* set pointer aliases */
+        tmpa = a->dp;
+        tmpb = b->dp;
+
+        /* carry */
+        r = 0;
+        for (x = 0; x < a->used; x++)
+        {
+            /**
+             * fetch the next carry from the MSB of the current digit
+             */
+            rr = *tmpa >> ((mp_digit)(MP_DIGIT_BIT - 1));
+            /* shift the current digit and add in the carry */
+            *tmpb++ = ((*tmpa++ << ((mp_digit)1)) | r) & MP_MASK;
+            /**
+             * propagate the carry to the next iteration
+             */
+            r = rr;
+        }
+        /**
+             * if there's a new leading digit add 1.
+             */
+        if (r != 0)
+        {
+            *tmpb = 1;
+            ++(b->used);
+        }
+        /**
+             * zero any leading digits
+             */
+        tmpb = b->dp + b->used;
+        for (x = b->used; x < oldused; x++)
+        {
+            *tmpb++ = 0;
+        }
+    }
+    b->sign = a->sign;
+    return MP_OKAY;
+}
+
+/* mp_div_2 implements division by two */
+int mp_div_2(mp_int *a, mp_int *b)
+{
+    int
+        x,
+        res, oldused;
+    /* copy */
+    if (b->alloc < a->used)
+    {
+        if ((res = mp_grow(b, a->used)) != MP_OKAY)
+        {
+            return res;
+        }
+    }
+    oldused = b->used;
+    b->used = a->used;
+    {
+        register mp_digit r, rr, *tmpa, *tmpb;
+        /* source alias */
+        tmpa = a->dp + b->used - 1;
+        /* dest alias */
+        tmpb = b->dp + b->used - 1;
+        /* carry */
+        r = 0;
+        for (x = b->used - 1; x >= 0; x--)
+        {
+            /* get the carry for the next iteration */
+            rr = *tmpa & 1;
+            /* shift the current digit, add in carry and store */
+            *tmpb-- = (*tmpa-- >> 1) | (r << (MP_DIGIT_BIT - 1));
+            /* forward carry to next iteration */
+            r = rr;
+        }
+        /* zero excess digits */
+        tmpb = b->dp + b->used;
+        for (x = b->used; x < oldused; x++)
+        {
+            *tmpb++ = 0;
+        }
+    }
+    b->sign = a->sign;
+    mp_clamp(b);
+    return MP_OKAY;
+}
